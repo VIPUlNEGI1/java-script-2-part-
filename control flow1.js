a38b543e3129c609651e24f73cc1487bbfd922e8
VIPUlNEGI1 <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-06-09 10:02:38 +0000

    control flow

--- control flow1.js
+++ control flow1.js
@@ -66,3 +66,13 @@ switch(month1){
 
             break;
 }
+
+// continue
+for(let index =1; index <=20; index++){
+    if (index ==5){
+        console.log (`Detected 5`)
+        continue
+    }
+    console.log(`Value of i is ${index}`)
+}
+// continue we are guesss by name continue is used to continue the  loop it is oppsite to the break statement.......
---
+++ loops.js
@@ -0,0 +1,52 @@
+// loop (iteration )
+
+// for
+
+
+// basic structor of for loop
+// let index =0; we declare the variable in for loop
+// index< Array.length; this is condation check
+// index++ incrment
+
+// for(let index = 0; index < Array.length; index++)
+//     {
+//         const element = array[index];
+//     }
+
+// for( let index=0; index<=10;index++){
+//     const element=index;
+//     console.log ('in' ,index)
+// }
+
+// for(let i =0 ; i<=10; i++){
+//     const vipul =i;
+//     if(vipul==5){
+//     console.log(i,"five is goood value")
+//     }
+
+//     console.log (i);
+// }
+
+// how to loop loop in for loop hahaaaaaaa nice one
+// for(let i=0; i<=10; i++ ){
+//     console.log(`Outer loop value : ${i}`)
+//     for(let j =0; j<=10; j++){
+//        console.log(`inner loop value${j} and inner loop ${i}`)
+//        console.log(i+'*'+j+'='+i*j);
+//     }
+// }
+
+// for(let i=1; i<=10; i++ ){
+//     console.log(`Outer loop value : ${i}`)
+//     for(let j =0; j<=10; j++){
+//      //  console.log(`inner loop value${j} and inner loop ${i}`)
+//        console.log(i+'*'+j+'='+i*j);
+//     }
+// }
+
+// let myArray =["vipul", 'abishik' ,'rohan', 'nitin','ram','shyam']
+// for(let index =0; index< myArray.length ; index++){
+//   const element = myArray[index]
+//   console.log(element)
+// }
+
