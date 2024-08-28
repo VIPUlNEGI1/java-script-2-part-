3f4a396e095bc2f94064975113853c95f185cb1c
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-08-28 08:17:51 +0000

    new commit

--- funcations2part.js
+++ funcations2part.js
@@ -6,10 +6,18 @@ console.log(calculateCarPrice(100))
 
 
 //this is a spacial condation to write the more values
+//... this is spread operator & restopertor
 function calculateCarPrice(...num1){
     return num1
 }
 console.log(calculateCarPrice(200,433,2323,232,212,100))
+
+
+// this is important for interview praspactiv
+function calculateCarPrice1(val1, val2 , ...num1){
+    return num1
+}
+console.log(calculateCarPrice1(200, 300, 100, 150))
 
 // how to used(pass) object in in funcation
 const user={
@@ -20,6 +28,11 @@ const user={
 function handleobjects(anyobject){
       console.log(`Username is${anyobject.username} and price si ${anyobject.price} `)
 }
+handleobjects(user)
+handleobjects({
+    username : "sam",
+    price : 399
+})
 
 // how to pass the array in funcation 
 const myNewArray =[200,400,100,600]
